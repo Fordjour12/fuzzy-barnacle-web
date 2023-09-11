8d8695f5643eda2b080d50403513bdb3847e571c
Fordjour12 <fordjourbobiem@gmail.com>
2023-09-11 12:14:29 +0000

    refactor(fuzzy-web): initial commit

---
+++ src/routes/authentication/(components)/user-auth-form.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	// import { Icons } from "$components/docs";
+    import { Button } from '$lib/components/ui/button';
+    import { Input } from "$lib/components/ui/input";
+    import { Label } from "$lib/components/ui/label";
+    import { cn } from "$lib/utils";
+    import {
+    	Loader2Icon
+    } from 'lucide-svelte';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+
+	let isLoading = false;
+	async function onSubmit() {
+		isLoading = true;
+
+		setTimeout(() => {
+			isLoading = false;
+		}, 3000);
+	}
+</script>
+
+<div class={cn("grid gap-6", className)} {...$$restProps}>
+	<form on:submit|preventDefault={onSubmit}>
+		<div class="grid gap-2">
+			<div class="grid gap-1">
+				<Label class="sr-only" for="email">Email</Label>
+				<Input
+					id="email"
+					placeholder="name@example.com"
+					type="email"
+					autocapitalize="none"
+					autocomplete="email"
+					autocorrect="off"
+					disabled={isLoading}
+				/>
+			</div>
+			<Button disabled={isLoading}>
+				{#if isLoading}
+					<Loader2Icon class="mr-2 h-4 w-4 animate-spin" />
+				{/if}
+				Sign In with Email
+			</Button>
+		</div>
+	</form>
+	<div class="relative">
+		<div class="absolute inset-0 flex items-center">
+			<span class="w-full border-t" />
+		</div>
+		<div class="relative flex justify-center text-xs uppercase">
+			<span class="bg-background px-2 text-muted-foreground">
+				Or continue with
+			</span>
+
+		</div>
+	</div>
+	 <Button variant="outline" type="button" disabled={isLoading}>
+		{#if isLoading}
+			<Loader2Icon class="mr-2 h-4 w-4 animate-spin" />
+		{:else}
+					<!-- <GithubIcon  name='github' class="mr-2 h-4 w-4"/> -->
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-album mr-2 h-4 w-4 animate-bounce"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><polyline points="11 3 11 11 14 8 17 11 17 3"/></svg>
+
+		{/if}
+		{" "}
+        Github
+	</Button>  
+</div>
